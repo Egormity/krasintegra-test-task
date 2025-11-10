b47945725a0b9af0d2891b9c82ed1cec844540bd
Egormity <kotlaregor9@gmail.com>
2025-11-10 11:27:44 +0700

    Upodate ErrorController

--- src/controllers/ErrorController.ts
+++ src/controllers/ErrorController.ts
@@ -7,7 +7,7 @@ export default class ErrorController {
 	static readonly handler = (err: any, req: Request, res: Response, next: NextFunction) => {
 		// Send general error if error is not operational in production
 		if (!err.isOperational && process.env.NODE_ENV === "production") {
-			utilSendResJson({ res, statusCode: 500, status: "error", message: "Something went wrong" });
+			utilSendResJson({ res, statusCode: 500, status: "fail", message: "Something went wrong" });
 			return;
 		}
 
@@ -15,9 +15,9 @@ export default class ErrorController {
 		utilSendResJson({
 			res,
 			statusCode: err.statusCode || 500,
-			status: err.status || "error",
-			message: err.message,
-			stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+			status: err.status || "fail",
+			message: err.message || "Something went wrong",
+			stack: process.env.NODE_ENV === "development" ? err.stack : undefined, // do not send the stack to production
 		});
 	};
 }
