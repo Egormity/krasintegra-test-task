ee68658851d46fc44f31e5709ea50d938bc63c7e
Egormity <kotlaregor9@gmail.com>
2025-11-11 15:27:27 +0700

    Added ControllerBaseHandler

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,74 +1,31 @@
-import catchAsync from "../utils/catchAsync";
-import sendResJson from "../utils/sendResJson";
-import AppError from "../utils/AppError";
 import prisma from "../lib/prisma";
+import ControllerBaseHandler from "../utils/ControllerBaseHandler";
 
-// In further development shared logic should be moved to a generic controller
+//
 export default class TaskController {
-	static readonly getMany = catchAsync(async (req, res) => {
-		const { completed, search, page = "1", size = "10" } = req.query;
-		const data = await prisma.task.findMany({
-			where: {
-				completed: completed ? (completed === "true" ? true : false) : undefined,
-				title: { contains: search as string | undefined },
-			},
-			take: +size,
-			skip: (+page - 1) * +size,
-		});
-		sendResJson({ res, data, statusCode: 200 });
+	static readonly getMany = ControllerBaseHandler.getMany({
+		prismaModel: prisma.task,
+		usePagination: true,
+		getWhere: ({ req }) => ({
+			title: { contains: req.query.search as string | undefined },
+			completed: req.query.completed ? (req.query.completed === "true" ? true : false) : undefined,
+		}),
 	});
-
-	//
-	static readonly getOneById = catchAsync(async (req, res, next) => {
-		const id = +req.params.id;
-		const data = await prisma.task.findUnique({ where: { id } });
-		if (!data) {
-			next(new AppError(404, `Item with id: "${id}" not found`));
-			return;
-		}
-		sendResJson({ res, statusCode: 200 });
+	static readonly getOneById = ControllerBaseHandler.getOneById({
+		prismaModel: prisma.task,
+		itemName: "Task",
 	});
-
-	//
-	static readonly postOne = catchAsync(async (req, res, next) => {
-		if (!req.body.title) return next(new AppError(400, "Title is required"));
-		const data = await prisma.task.create({ data: req.body });
-		sendResJson({ res, data, statusCode: 201, message: "Task created successfully" });
+	static readonly postOne = ControllerBaseHandler.postOne({ prismaModel: prisma.task, itemName: "Task" });
+	static readonly putOneById = ControllerBaseHandler.putOneById({
+		prismaModel: prisma.task,
+		itemName: "Task",
 	});
-
-	//
-	static readonly putOneById = catchAsync(async (req, res, next) => {
-		const id = +req.params.id;
-		const existing = await prisma.task.findUnique({ where: { id } });
-		if (!existing) {
-			next(new AppError(404, `Item with id: "${id}" not found`));
-			return;
-		}
-		const data = await prisma.task.update({ where: { id }, data: req.body });
-		sendResJson({ res, data, statusCode: 201, message: "Task updated successfully" });
+	static readonly patchOneById = ControllerBaseHandler.patchOneById({
+		prismaModel: prisma.task,
+		itemName: "Task",
 	});
-
-	//
-	static readonly patchOneById = catchAsync(async (req, res, next) => {
-		const id = +req.params.id;
-		const existing = await prisma.task.findUnique({ where: { id } });
-		if (!existing) {
-			next(new AppError(404, `Item with id: "${id}" not found`));
-			return;
-		}
-		const data = await prisma.task.update({ where: { id }, data: { ...existing, ...req.body } });
-		sendResJson({ res, data, statusCode: 200, message: "Task updated successfully" });
-	});
-
-	//
-	static readonly deleteOneById = catchAsync(async (req, res, next) => {
-		const id = +req.params.id;
-		const existing = await prisma.task.findUnique({ where: { id } });
-		if (!existing) {
-			next(new AppError(404, `Item with id: "${id}" not found`));
-			return;
-		}
-		await prisma.task.delete({ where: { id } });
-		sendResJson({ res, statusCode: 204, message: "Task deleted successfully" });
+	static readonly deleteOneById = ControllerBaseHandler.deleteOneById({
+		prismaModel: prisma.task,
+		itemName: "Task",
 	});
 }
---
+++ src/utils/ControllerBaseHandler.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from "express";
+
+import AppError from "./AppError";
+import catchAsync from "./catchAsync";
+import sendResJson from "./sendResJson";
+
+//
+export default class ControllerBaseHandler {
+	static readonly getMany = ({
+		prismaModel,
+		usePagination = false,
+		getWhere,
+	}: {
+		prismaModel: any;
+		usePagination?: boolean;
+		getWhere?: ({ req, res }: { req: Request; res: Response }) => any;
+	}) =>
+		catchAsync(async (req, res) => {
+			const { page = "1", size = "10" } = req.query;
+			const data = await prismaModel.findMany({
+				where: getWhere?.({ req, res }),
+				take: usePagination ? +size : undefined,
+				skip: usePagination ? (+page - 1) * +size : undefined,
+			});
+			sendResJson({ res, data, statusCode: 200 });
+		});
+
+	//
+	static readonly getOneById = ({
+		prismaModel,
+		itemName = "Item",
+	}: {
+		prismaModel: any;
+		itemName?: string;
+	}) =>
+		catchAsync(async (req, res, next) => {
+			const id = +req.params.id;
+			const data = await prismaModel.findUnique({ where: { id } });
+			if (!data) {
+				next(new AppError(404, `${itemName} with id: "${id}" not found`));
+				return;
+			}
+			sendResJson({ res, data, statusCode: 200 });
+		});
+
+	//
+	static readonly postOne = ({ prismaModel, itemName = "Item" }: { prismaModel: any; itemName?: string }) =>
+		catchAsync(async (req, res, next) => {
+			if (!req.body.title) return next(new AppError(400, "Title is required"));
+			const data = await prismaModel.create({ data: req.body });
+			sendResJson({ res, data, statusCode: 201, message: `${itemName} created successfully` });
+		});
+
+	//
+	static readonly putOneById = ({
+		prismaModel,
+		itemName = "Item",
+	}: {
+		prismaModel: any;
+		itemName?: string;
+	}) =>
+		catchAsync(async (req, res, next) => {
+			const id = +req.params.id;
+			const existing = await prismaModel.findUnique({ where: { id } });
+			if (!existing) {
+				next(new AppError(404, `${itemName} with id: "${id}" not found`));
+				return;
+			}
+			const data = await prismaModel.update({ where: { id }, data: req.body });
+			sendResJson({ res, data, statusCode: 201, message: `${itemName} updated successfully` });
+		});
+
+	//
+	static readonly patchOneById = ({
+		prismaModel,
+		itemName = "Item",
+	}: {
+		prismaModel: any;
+		itemName?: string;
+	}) =>
+		catchAsync(async (req, res, next) => {
+			const id = +req.params.id;
+			const existing = await prismaModel.findUnique({ where: { id } });
+			if (!existing) {
+				next(new AppError(404, `${itemName} with id: "${id}" not found`));
+				return;
+			}
+			const data = await prismaModel.update({ where: { id }, data: { ...existing, ...req.body } });
+			sendResJson({ res, data, statusCode: 200, message: `${itemName} updated successfully` });
+		});
+
+	//
+	static readonly deleteOneById = ({
+		prismaModel,
+		itemName = "Item",
+	}: {
+		prismaModel: any;
+		itemName?: string;
+	}) =>
+		catchAsync(async (req, res, next) => {
+			const id = +req.params.id;
+			const existing = await prismaModel.findUnique({ where: { id } });
+			if (!existing) {
+				next(new AppError(404, `${itemName} with id: "${id}" not found`));
+				return;
+			}
+			await prismaModel.delete({ where: { id } });
+			sendResJson({ res, statusCode: 204, message: `${itemName} deleted successfully` });
+		});
+}
